9ce78badce7fa056be53503cbe8bd37747095c67
minaripenguin <minaripenguin@users.noreply.github.com>
2024-06-12 09:03:41 +0800

    SystemUI: Introduce lockscreen media art filter [1/2]
    * Alternative to lockscreen media feature which Google deprecated
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -88,6 +88,7 @@ import android.view.MotionEvent;
 import android.view.ThreadedRenderer;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewGroup;
 import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
@@ -244,6 +245,7 @@ import com.android.systemui.statusbar.window.StatusBarWindowStateController;
 import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.DumpUtilsKt;
+import com.android.systemui.util.MediaArtUtils;
 import com.android.systemui.util.WallpaperController;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.concurrency.MessageRouter;
@@ -457,6 +459,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final NotificationsController mNotificationsController;
     private final StatusBarSignalPolicy mStatusBarSignalPolicy;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
+
+    private final MediaArtUtils mMediaArtUtils;
 
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
@@ -914,6 +918,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
 
         mWindowManager = windowManager;
         mWindowManagerProvider = windowManagerProvider;
+
+        mMediaArtUtils = MediaArtUtils.getInstance(mContext);
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1160,6 +1166,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                 (requestTopUi, componentTag) -> mMainExecutor.execute(() ->
                         mNotificationShadeWindowController.setRequestTopUi(
                                 requestTopUi, componentTag))));
+		getNotifContainerParentView().addView(mMediaArtUtils.getMediaArtScrim(), 0);
+    }
+    
+    
+    private ViewGroup getNotifContainerParentView() {
+        ViewGroup rootView = (ViewGroup) getNotificationShadeWindowView().findViewById(R.id.scrim_behind).getParent();
+        ViewGroup targetView = rootView.findViewById(R.id.notification_container_parent);
+        return targetView;
     }
 
     @VisibleForTesting
@@ -1412,7 +1426,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             });
         }
 
-        mVisualizerView = (VisualizerView) getNotificationShadeWindowView().findViewById(R.id.visualizerview);
+        mVisualizerView = (VisualizerView) getNotifContainerParentView().findViewById(R.id.visualizerview);
 
         mReportRejectedTouch = getNotificationShadeWindowView()
                 .findViewById(R.id.report_rejected_touch);
@@ -2769,6 +2783,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             }
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            mMediaArtUtils.updateMediaArtVisibility();
             if (Settings.System.getIntForUser(mContext.getContentResolver(),
                                               Settings.System.ARCANE_IDLE_MANAGER, 1,
                                               mLockscreenUserManager.getCurrentUserId()) == 1) {
